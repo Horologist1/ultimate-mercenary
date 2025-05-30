bb3df01baa03e955474ce687a3d8b653151efe6c
Horologist1 <mcolodrog@gmail.com>
2025-05-30 16:03:07 +0200

    0.86

--- character-sheet.js
+++ character-sheet.js
@@ -498,7 +498,7 @@ function updateCharacterSheetInFirebase(data) {
             ...data,
             // Timestamp de guardado
             lastSaved: new Date().toISOString(),
-            version: '0.85'
+            version: '0.86'
         }).then(() => {
             console.log('✅ Ficha guardada en Firebase');
         }).catch(error => {
